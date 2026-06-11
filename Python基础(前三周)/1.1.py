054a64d7aba3299f2addb4fc1d47836d6ff7cdb8
xman661 <3495335726@qq.com>
2026-06-11 19:28:26 +0800

    前3周: Python基础(1.1)

---
+++ Python基础(前三周)/1.1.py
@@ -0,0 +1,25 @@
+#Step 3
+print('hello robort')
+#STep 4
+name='小明'
+age=20
+height=1.75
+print('你好')
+print('我今年',age,'岁')
+print('我今年',type(age),'岁')
+print(f'我今年{age}岁')
+name1 = input('')
+print(f'你好，{name1}')
+# 练习1
+name3='徐满'
+age=20
+print(f'我叫{name3},今年{age}')
+# 练习2
+a=int(input(''))
+b=int(input(''))
+c=(f'两数之和，{a+b}')
+print(c)
+# 练习3: 摄氏温度转华氏度
+d=int(input(''))
+e=d*9/5+32
+print(f'{d}°C ={e}°F')
